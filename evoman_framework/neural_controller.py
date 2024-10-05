d7360ef887a2b61aec142f7b153868506efa48f1
MR <mikaros3@gmail.com>
2024-10-05 20:41:17 +0200

    Changed internal neural controller layout
    - layout is now equivalent to the provided demo controller (same results!)
    - equivalence is ensured over pytest

--- evoman_framework/neural_controller.py
+++ evoman_framework/neural_controller.py
@@ -14,7 +14,7 @@ def sigmoid_activation(x: np.ndarray) -> np.ndarray:
     """
     return 1./(1. + np.exp(-x))
 
-
+# TODO: Synchronize structure of own neural controller with the pre-implemented one
 # implements controller structure for player
 class NeuralController(Controller):
     """Neural network based controller. For simplicity always has one hidden layer."""
@@ -46,24 +46,28 @@ class NeuralController(Controller):
         """Sets the GENOME of the controller.
 
         Args:
-            genome (list): Genome, being updated
+            genome (list): Genome being updated.
             _ (): Unused parameter.
         """
         assert self.get_genome_size() == len(genome), ValueError(
             f"The length of the given genome ({len(genome)}, does not "\
             f"align with the expected size ({self.get_genome_size()}))"
         )
+
         if not isinstance(genome, np.ndarray):
             genome = np.array(genome)
-        assert isinstance(genome, np.ndarray)
-        w1_end = self.w1_matrix.size
-        w2_end = self.w1_matrix.size + self.w2_matrix.size
-        bias1_end = w2_end + self.bias1.size
 
-        self.w1_matrix = genome[:w1_end].reshape(self.w1_matrix.shape)
-        self.w2_matrix = genome[w1_end:w2_end].reshape(self.w2_matrix.shape)
-        self.bias1 = genome[w2_end:bias1_end].reshape(self.bias1.shape)
-        self.bias2 = genome[bias1_end:].reshape(self.bias2.shape)
+        assert isinstance(genome, np.ndarray)
+
+        b1_end = self.bias1.size
+        w1_end = b1_end + self.w1_matrix.size
+        b2_end = w1_end + self.bias2.size
+        w2_end = b2_end + self.w2_matrix.size
+
+        self.bias1 = genome[:b1_end].reshape(self.bias1.shape)
+        self.w1_matrix = genome[b1_end:w1_end].reshape(self.w1_matrix.shape)
+        self.bias2 = genome[w1_end:b2_end].reshape(self.bias2.shape)
+        self.w2_matrix = genome[b2_end:w2_end].reshape(self.w2_matrix.shape)
 
     def control(self, params: list, cont=None) -> list[bool]:
         """Method being called with the current input from the game (states).
@@ -71,22 +75,20 @@ class NeuralController(Controller):
         move. The values are determined by the Neural Network.
 
         Args:
-            params (list): inputs (usually 20 sensor inputs)
-            cont (any): Unused parameter
+            params (list): Inputs (usually 20 sensor inputs).
+            cont (any): Unused parameter.
 
         Returns:
-            list[bool]: Actions in the following order: [left, right, jump, shoot, release]
+            list[bool]: Actions in the following order: [left, right, jump, shoot, release].
         """
         inputs = np.expand_dims(np.array(params), axis=0)
         # Perform min-max normalization over inputs (optional)
         inputs = (inputs - inputs.min()) / (inputs.max() - inputs.min()).astype(np.float64)
-
         # input -> hidden
         output = sigmoid_activation(inputs.dot(self.w1_matrix) + self.bias1)
-
         # hidden -> output
         output = sigmoid_activation(output.dot(self.w2_matrix)+ self.bias2)[0]
-
         # thresholding the output with 0.5
         output = np.where(output > 0.5, 1, 0)
+
         return output.tolist()
---
+++ evoman_framework/test_controller_equivalence.py
@@ -0,0 +1,30 @@
+from evoman_framework.neural_controller import NeuralController
+from demo_controller import player_controller
+import numpy as np
+
+
+def test_controller_equivalence():
+    np.random.seed(42)
+    # test 3 just in case
+    genome = np.random.uniform(low=-10, high=10, size=265)
+    genome2 = np.random.uniform(low=-10, high=10, size=265)
+    genome3 = np.random.uniform(low=-10, high=10, size=265)
+
+    nc = NeuralController(
+        n_inputs=20,
+        n_outputs=5,
+        hidden_size=10
+    )
+    org_controller = player_controller(10)
+    inputs = list(range(20))
+
+    for genome in [genome, genome2, genome3]:
+        nc.set(genome)
+        nc_result = nc.control(inputs)
+
+        org_controller.set(genome, 20)
+        org_result = org_controller.control(np.array(inputs), None)
+
+        assert (nc_result == org_result), f"Results where not equal: {nc_result}!={org_result}"
+
+    
