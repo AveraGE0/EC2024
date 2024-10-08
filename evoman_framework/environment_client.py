8b6a9ad050521549a5cf8c029e40457633286321
MR <mikaros3@gmail.com>
2024-10-08 14:47:24 +0200

    Changed parallel computation components to take custom fitness function.
    - added 'fintess_func' parameter in parallel environment and client

--- evoman_framework/environment_client.py
+++ evoman_framework/environment_client.py
@@ -1,21 +1,29 @@
 """Module for specifying a process (function) able to run an environment and
 simulate individuals in it."""
 import os
+import types
 import multiprocessing as mp
 from evoman.environment import Environment
 from neural_controller import NeuralController
 
 
-def env_process(worker_id: int, task_queue: mp.Queue, result_queue: mp.Queue, config: dict) -> None:
+def env_process(
+        worker_id: int,
+        task_queue: mp.Queue,
+        result_queue: mp.Queue,
+        config: dict,
+        fitness_func: callable
+    ) -> None:
     """Function for process running environment for simulation.
     Environment is created, the function waits for new tasks in the
     queue and works on them as soon as they are scheduled.
 
     Args:
-        worker_id (int): id of the process running this function
-        task_queue (mp.Queue): Queue for open tasks (individual evaluation)
-        result_queue (mp.Queue): Queue where results will be stored
-        config (dict): COnfiguration as dict
+        worker_id (int): id of the process running this function.
+        task_queue (mp.Queue): Queue for open tasks (individual evaluation).
+        result_queue (mp.Queue): Queue where results will be stored.
+        config (dict): Configuration as dict.
+        fitness_func (callable): The fitness function used in the environment.
     """
     print(f"Worker {worker_id} starting, initializing environment...")
 
@@ -39,9 +47,25 @@ def env_process(worker_id: int, task_queue: mp.Queue, result_queue: mp.Queue, co
         visuals=False,
         level=config["level"],
     )
+    # this function transforms the output vectors to z-scores we want the actual scores, we
+    # change it to return the original values
+    env.cons_multi = lambda x: x
+    # adjust the fitness calculated per enemy
+    env.fitness_single = types.MethodType(fitness_func, env)
+    #env.fitness_single = fitness_func
 
-    # wait for tasks and run them
     while True:
         idx, sub_population = task_queue.get()
-        # Send the result back to the parent process via result queue
-        result_queue.put((idx, [env.play(pcont=genome) for genome in sub_population]))
+        # Send the result back to the parent process via result queue as a list of dicts
+        # so the results are easier to understand!
+        results = []
+        for genome in sub_population:
+            result = env.play(pcont=genome)
+            results.append({
+                "fitness": result[0],
+                "player_life": result[1],
+                "enemy_life": result[2],
+                "time": result[3]
+            })
+
+        result_queue.put((idx, results))
--- evoman_framework/parallel_environment.py
+++ evoman_framework/parallel_environment.py
@@ -2,28 +2,29 @@
 to provide a multithreading version that is easily usable"""
 import os
 import multiprocessing
-import yaml
 import numpy as np
 from environment_client import env_process
 
 
 class ParallelEnvironment:
     """
-    Class the allows to run an arbitrary number of processes running the environment.
+    Class the allows to run an arbitrary number of processes running the environment
+    in parallel.
     """
-    def __init__(self, n_processes: int, config_path: str) -> None:
+    def __init__(self, n_processes: int, config: dict, fitness_func: callable) -> None:
         """Init method
 
         Args:
             n_processes (int): amount of processes created
-            config_path (str): path to the configuration used within all environments
+            config (dict): the configuration parameters used within all environments
+            fitness_func (callable): fitness function that is used to calculate the fitness array
         """
         # disable any video from opening
         os.environ["SDL_VIDEODRIVER"] = "dummy"
 
-        with open(config_path, "r", encoding="utf-8") as f:
-            self.config = yaml.safe_load(f)
+        self.config = config
 
+        self.fitness_func = fitness_func
         self.processes = []
         self.task_queue = multiprocessing.Queue()
         self.result_queue = multiprocessing.Queue()
@@ -34,10 +35,11 @@ class ParallelEnvironment:
         for i in range(self.n_processes):
             p = multiprocessing.Process(
                 target=env_process,
-                args=(i, self.task_queue, self.result_queue, self.config)
+                args=(i, self.task_queue, self.result_queue, self.config, self.fitness_func)
             )
             p.start()
             self.processes.append(p)
+
         print(f"Started {self.n_processes} worker processes!")
 
     def queue_tasks(self, sub_populations: list[list]) -> None:
@@ -53,18 +55,20 @@ class ParallelEnvironment:
         """Terminates all processes previously created"""
         for process in self.processes:
             process.terminate()
+
         print("Closed all subprocesses successfully.")
 
     def get_results(self, population: list) -> list:
         """Method to asynchronously (parallel) get results on a population using
-        multiple threads.
+        multiple processes.
 
         Args:
             population (list): population being evaluated.
 
         Returns:
             list: returns a list with all evaluation scores obtained from
-            calling env.play()
+            calling env.play(). Results are in form of a dict with
+            fitness, player_life, enemy_life, time keys.
         """
         # split and queue data for processing
         splits = np.array_split(population, self.n_processes)
@@ -78,4 +82,5 @@ class ParallelEnvironment:
         merged_results = []
         for _, partial_result in results:
             merged_results = merged_results + partial_result
+
         return merged_results
